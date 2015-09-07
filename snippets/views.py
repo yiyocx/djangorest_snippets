41304df48dcb7ea16a7f49fa8278b9f258456516
Yiyo Castillo <yiyocx@gmail.com>
2015-09-07 17:31:43 -0500

    Se cambian function based views por class based views
    Para manejar la respuesta de la api

--- snippets/views.py
+++ snippets/views.py
@@ -1,9 +1,51 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import Http404
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
+# Class Based Views ----------------------------------------------------------------
+class SnippetList(APIView):
+    def get(self, request, format=None):
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUESTH)
+
+class SnippetDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = SnippetSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NOT_CONTENT)
+
+# Function Based Views --------------------------------------------------------------
 @api_view(['GET', 'POST'])
 def snippet_list(request, format=None):
     """
