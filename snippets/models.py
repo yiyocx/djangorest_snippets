2b7c42a912c6947d1a9eaf90e1cf3b5591247c26
Yiyo Castillo <yiyocx@gmail.com>
2015-09-08 15:47:42 -0500

    Agregado modelo de Usuarios para los Snippets
    Tambien se agregan endpoint para usuarios y sus respectivas vistas

--- snippets/models.py
+++ snippets/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
+from pygments.lexers import get_lexer_by_name
+from pygments import highlight
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
@@ -13,6 +15,20 @@ class Snippet(models.Model):
 	linenos = models.BooleanField(default=False)
 	language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
 	style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+	owner = models.ForeignKey('auth.User', related_name='snippets')
+	highlighted = models.TextField()
 
 	class Meta:
 		ordering = ('created',)
+
+	def save(self, *args, **kwargs):
+		"""
+		Se usa la libreria pygments para crear una representacion del 
+		codigo del Snippet con highlight HTML
+		"""
+		lexer = get_lexer_by_name(self.language)
+		linenos = self.linenos and 'table' or False
+		options = self.title and {'title': self.title} or {}
+		formatter = HtmlFormatter(style=self.style, linenos=linenos, full=True, **options)
+		self.highlighted = highlight(self.code, lexer, formatter)
+		super(Snippet, self).save(*args, **kwargs)
--- snippets/views.py
+++ snippets/views.py
@@ -1,17 +1,30 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from snippets.serializers import UserSerializer
 from rest_framework.decorators import api_view
 from rest_framework import mixins
 from rest_framework import generics
+from django.contrib.auth.models import User
 
 # Usando Mixin Generics ----------------------------------------------------------------
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 # Function Based Views --------------------------------------------------------------
 @api_view(['GET', 'POST'])
