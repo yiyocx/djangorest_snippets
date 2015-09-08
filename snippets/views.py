bfc32b16a933b65b476c340ecd45f8d78f8b59fb
Yiyo Castillo <yiyocx@gmail.com>
2015-09-08 07:59:52 -0500

    Se cambian mixins por generic mixins
    Con esto se logra manejar las respuestas del endpoint /snippets con aun menos código

--- snippets/views.py
+++ snippets/views.py
@@ -1,31 +1,17 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from rest_framework.decorators import api_view
 from rest_framework import mixins
 from rest_framework import generics
 
-# Usando Mixins ----------------------------------------------------------------
-class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+# Usando Mixin Generics ----------------------------------------------------------------
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):    
-        return self.create(request, *args, **kwargs)
-
-class SnippetDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
 
 # Function Based Views --------------------------------------------------------------
 @api_view(['GET', 'POST'])
